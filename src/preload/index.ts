deccd4989eaab7500ad7e7883ec68903b315a222
Kamil So <sowicz09@gmail.com>
2025-02-01 16:14:39 +0100

    removed error in build

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -6,7 +6,7 @@ contextBridge.exposeInMainWorld("electron", {
     send: (channel, data) => ipcRenderer.send(channel, data),
 
     on: (channel, callback) =>
-      ipcRenderer.on(channel, (event, ...args) => callback(...args)),
+      ipcRenderer.on(channel, (_, ...args) => callback(...args)),
 
     off: (channel, callback) =>
       ipcRenderer.removeListener(channel, callback), // This prevents memory leaks
